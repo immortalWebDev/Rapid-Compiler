7733d77f3ece8c9c07c6c3a43ef221d7e413c218
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-27 18:43:01 +0530

    Added the output window (success)

---
+++ src/components/ResultWindow.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ResultWindow = ({ outputDetails }) => {
+  const getOutput = () => {
+    let statusId = outputDetails?.status?.id;
+
+    if (statusId === 3) {
+      return (
+        <pre className="px-2 py-1 font-normal text-xs text-green-400">
+          {atob(outputDetails.stdout) !== null
+            ? `${atob(outputDetails.stdout)}`
+            : null}
+        </pre>
+      );
+    }
+  };
+  return (
+    <>
+      <h1 className="">Output here:</h1>
+      <div className="w-full h-50 ">
+        {outputDetails ? <>{getOutput()}</> : null}
+      </div>
+    </>
+  );
+};
+
+export default ResultWindow;
