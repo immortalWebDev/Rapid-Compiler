af5bcbb085cfb9205f2caf2fa5c0cfa5500e3158
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-27 17:56:52 +0530

    Added Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="">
+      <div className="logo-container">
+        <h1 className="logo-text text-2xl">Rapid Compiler</h1>
+      </div>
+
+      <div className="nav-links-container">
+        <p className="nav-item text-center">Coding platform by Me</p>
+      </div>
+    </div>
+  );
+}
