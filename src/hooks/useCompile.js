9069c3a7b8f0d8c61d6c3fd176069518647ceee9
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-31 17:06:02 +0530

    Implementd error handling for fetch and checkStatus functionality

--- src/hooks/useCompile.js
+++ src/hooks/useCompile.js
@@ -2,10 +2,18 @@ import { useState } from "react";
 import axios from "axios";
 import { Buffer } from "buffer";
 
-const useCompile = (language, code) => {
+const useCompile = (
+  language,
+  code,
+  setOutputDetails,
+  showErrorToast,
+  showSuccessToast
+) => {
   const [processing, setProcessing] = useState(false);
 
+  // Function to handle compilation
   const handleCompile = async () => {
+
     setProcessing(true);
 
     const formData = {
@@ -14,7 +22,7 @@ const useCompile = (language, code) => {
     };
 
     try {
-      // axios post for req
+      // Making a POST request for compiling code
       const response = await axios.post(
         import.meta.env.VITE_RAPID_URL,
         formData,
@@ -29,8 +37,54 @@ const useCompile = (language, code) => {
       );
 
       console.log("response received from server", response.data);
+
+      const token = response.data.token;
+      await checkStatus(token); // Check the status of the compilation
+
     } catch (error) {
-      console.log(error);
+      const status = error.response?.status;
+      console.log("status", status);
+
+      if (status === 429) {
+        showErrorToast("Quota of 100 requests exceeded for the Day!", 10000);
+      } else {
+        showErrorToast("Something went wrong. Try again!");
+      }
+
+      setProcessing(false);
+    }
+  };
+
+  // Function to check the status of compilation
+  const checkStatus = async (token) => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_RAPID_URL}/${token}`,
+        {
+          params: { base64_encoded: "true", fields: "*" },
+          headers: {
+            "X-RapidAPI-Host": import.meta.env.VITE_RAPID_HOST,
+            "X-RapidAPI-Key": import.meta.env.VITE_RAPID_API_KEY,
+          },
+        }
+      );
+
+      const statusId = response.data.status?.id;
+
+      if (statusId === 1 || statusId === 2) {
+        // Status 1 or 2 means the code is still processing, so poll again after 2 seconds
+        setTimeout(() => checkStatus(token), 2000);
+      } else {
+        setProcessing(false);
+        setOutputDetails(response.data); // Set the final output details
+        console.log(response.data)
+        showSuccessToast("Compiled Successfully!");
+      }
+    } catch (err) {
+      console.log("Error while checking status", err);
+
+      showErrorToast("Something went wrong while checking status!");
+      setProcessing(false);
     }
   };
 
