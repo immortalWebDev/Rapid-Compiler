f604652fd0541efbbaec1d0fffe2bcc647959906
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-31 17:10:05 +0530

    Added simple footer with portfolio link

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => (
+    <footer>   
+      <p className="flex justify-center items-center text-gray-800 font-normal text-sm pt-2.5">
+        Crafted with 💝 by<a className="text-gray-800 font-semibold" href="https://web-portfolio-piyush.vercel.app/" target="_blank">&nbsp;Piyush</a>
+      </p> 
+  </footer>
+);
+
+
+export default Footer;
