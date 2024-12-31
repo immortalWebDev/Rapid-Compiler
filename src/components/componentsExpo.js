8d9b00de2ae73d2f152dda4cb35e549e823e5ff5
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-31 18:08:30 +0530

    Export files

---
+++ src/components/componentsExpo.js
@@ -0,0 +1,10 @@
+export { default as Navbar } from './Navbar';
+export { default as CodeEditor } from './CodeEditor';
+export { default as HomePage } from './HomePage';
+// export { default as InputField } from './InputField';
+export { default as LanguagesDropdown } from './LanguagesDropdown';
+export { default as ResultDetails } from './ResultDetails';
+export { default as ResultWindow } from './ResultWindow';
+export { default as Footer } from './Footer';
+export { default as ThemeSelector } from './ThemeSelector';
+
---
+++ src/hooks/hooksExpo.js
@@ -0,0 +1,5 @@
+export {default as useCompile} from "./useCompile"
+export {default as useTheme} from "./useTheme"
+// export {default as useShortcutKey} from "./useShortcutKey"
+
+
