7da2775237e54646e630679c243cfa91b1161cf2
immortalWebDev <pgbadgujar007@gmail.com>
2025-01-01 18:00:13 +0530

    Updated result details panel with stats from api showcase

--- src/components/ResultDetails.jsx
+++ src/components/ResultDetails.jsx
@@ -2,22 +2,27 @@ import React from "react";
 
 const ResultDetails = ({ outputDetails }) => {
   return (
-    <div className="">
-      <p className="text-sm font-medium text-gray-700">
-        Status:{outputDetails?.status?.description}
-      </p>
-
-      <p className="text-sm font-medium text-gray-700">
-        Memory:{outputDetails?.memory}
-      </p>
-
-      <p className="text-sm font-medium text-gray-700">
-        Time:{outputDetails?.time}
-      </p>
+    <div className="metrics-container mt-4 flex flex-col space-y-4 p-4 bg-gray-50 rounded-lg shadow-lg border-2 border-blue-500 relative">
+      <div className="flex justify-between items-center">
+        <p className="text-sm font-medium text-gray-700">Status:&nbsp;</p>
+        <span className="px-2 py-1 rounded-md bg-indigo-600 text-white text-xs font-semibold">
+          {outputDetails?.status?.description}
+        </span>
+      </div>
+      <div className="flex justify-evenly items-center">
+        <p className="text-sm font-medium text-gray-700">Memory:</p>
+        <span className="px-1 py-1 rounded-md bg-green-600 text-white text-xs font-semibold">
+          {outputDetails?.memory}
+        </span>
+      </div>
+      <div className="flex justify-evenly items-center">
+        <p className="text-sm font-medium text-gray-700">Time:</p>
+        <span className="px-2 py-1 rounded-md bg-purple-600 text-white text-xs font-semibold">
+          {outputDetails?.time}
+        </span>
+      </div>
     </div>
   );
 };
 
 export default ResultDetails;
-
-//till here
