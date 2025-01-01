74318ff95f606aa117379fed46562b625d05b2e7
immortalWebDev <pgbadgujar007@gmail.com>
2025-01-01 17:44:09 +0530

    Used editor and result window and updated styles (WEP)

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -20,12 +20,35 @@ const HomePage = () => {
         </div>
       </div>
 
-      <div className="flex flex-row">
-        <div className="">
-          <CodeEditor code={code} onChange={onChange} />
+      <div className="flex flex-row space-x-4 items-start px-4 pt-4 flex-wrap md:flex-nowrap">
+        <div className="flex flex-col w-full h-full justify-start items-end">
+          <CodeEditor
+            code={code}
+            onChange={onChange}
+            language={language?.value}
+            theme={theme.value}
+          />
         </div>
 
-        <div className=""></div>
+        <div className="right-container flex flex-shrink-0 sm:w-[30%] flex-col">
+
+          <ResultWindow outputDetails={outputDetails} />
+          <div className="flex flex-col items-start">
+            <div className="flex items-center gap-12">
+              <button
+                onClick={handleCompile}
+                disabled={!code}
+                className={`mt-4 w-full  min-w-[200px] border-2 border-indigo-600 text-indigo-600 rounded-md px-6 py-3 font-medium transition-all duration-200 ease-in-out hover:bg-indigo-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-opacity-50 ${
+                  !code ? "opacity-50 cursor-not-allowed" : ""
+                }`}
+              >
+                {processing ? "Processing..." : "Compile and Execute"}
+              </button>
+
+              {outputDetails && <ResultDetails outputDetails={outputDetails} />}
+            </div>
+          </div>
+        </div>
       </div>
 
       {/* footer here */}
