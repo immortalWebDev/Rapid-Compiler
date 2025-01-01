40ce11c05fa4c27dd7ea0e70a0e4aa6193d3dc00
immortalWebDev <pgbadgujar007@gmail.com>
2025-01-01 18:00:57 +0530

    Success result fetch logic

--- src/components/ResultWindow.jsx
+++ src/components/ResultWindow.jsx
@@ -4,7 +4,14 @@ const ResultWindow = ({ outputDetails }) => {
   const getOutput = () => {
     let statusId = outputDetails?.status?.id;
 
-    if (statusId === 3) {
+    if (statusId === 6) {
+      
+      return (
+        <pre className="px-2 py-1 font-normal text-xs text-red-400">
+          {atob(outputDetails?.compile_output)}
+        </pre>
+      );
+    } else if (statusId === 3) {
       return (
         <pre className="px-2 py-1 font-normal text-xs text-green-400">
           {atob(outputDetails.stdout) !== null
