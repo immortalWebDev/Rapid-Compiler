efa92bfddaa2007ad65987f64d15549e6e6c36b0
immortalWebDev <pgbadgujar007@gmail.com>
2025-01-01 17:50:10 +0530

    Updated nav styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,16 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div className="">
-      <div className="logo-container">
-        <h1 className="logo-text text-2xl">Rapid Compiler</h1>
+    <div className="navbar-container w-full h-16 flex items-center justify-between px-8 text-white shadow-xl bg-gradient-to-r from-blue-500 to-indigo-600 animate-gradientBackground">
+      <div className="logo-container flex-shrink-0 flex items-center justify-center bg-white text-indigo-600 rounded-lg p-2 shadow-md">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/1828/1828231.png"
+          alt="ico"
+          className="w-9 h-9 p-1"
+        />
+        <h1 className="logo-text text-2xl font-bold font-mono tracking-tight">
+          Rapid Compiler
+        </h1>
       </div>
 
       <div className="nav-links-container">
